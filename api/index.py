b8e34c2ac359642609c14c522e293363a1f345ae
Wok Flinn Mau Femes <roflin2008@gmail.com>
2026-02-09 19:27:41 +0700

    Update YouTube DL options in index.py
    Replaced 'source_address' option with 'user_agent' for improved connection handling.

--- api/index.py
+++ api/index.py
@@ -98,7 +98,7 @@ def stream(yt_id):
         'no_warnings': True,
         'nocheckcertificate': True,
         'noplaylist': True,
-        'source_address': '0.0.0.0', # Menghindari beberapa masalah koneksi di server
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
     with yt_dlp.YoutubeDL(opts) as ydl:
         try:
